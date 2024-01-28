671b2700a7e69f271e42abafcfb635a04631d458
BROWN <90845793+qxbrown@users.noreply.github.com>
2024-01-28 12:46:20 +0545

    Add files via upload

--- Thunar/accels.scm
+++ Thunar/accels.scm
@@ -80,6 +80,7 @@
 ; (gtk_accel_path "<Actions>/ThunarWindow/preferences" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/switch-previous-tab" "<Primary>Page_Up")
 ; (gtk_accel_path "<Actions>/ThunarWindow/zoom-in-alt1" "<Primary>KP_Add")
+; (gtk_accel_path "<Actions>/ThunarBookmarks/c1c6a33d177a5e3a6fb51ee7a0031ea7" "")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/toggle-sort-order" "")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/open-in-new-window" "<Primary><Shift>o")
 ; (gtk_accel_path "<Actions>/ThunarWindow/view-configure-toolbar" "")
